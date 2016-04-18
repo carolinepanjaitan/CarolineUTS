e0451fdf4f6fca67458a33d76d308f0e13069866
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-04-18 11:06:29 +0700

    UTS

---
+++ Batas.java
@@ -0,0 +1,11 @@
+double[] a = new double[100];
+    
+    public void Input() {
+        
+        for ( int i = 1; i <=-999; i--) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Masukkan nilai IP: ");
+            a[i] = input.nextDouble();
+        }
+    }
+    
