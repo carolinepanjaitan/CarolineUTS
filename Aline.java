af0618d31415368c730879e8cf2fd73b5f0db8c4
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-04-18 11:00:16 +0700

    UTS

---
+++ Aline.java
@@ -0,0 +1,15 @@
+for (i = 1; i <= ip; i++) {
+        if (ip >= 2.75 && ip <= 4.00)
+            System.out.println("Lulus");
+        else
+            System.out.println("Data IP tidak valid");
+    }
+
+
+
+if(ip[i-1] >= 2.75 && <= 4.00) {
+                jumlahLulus=jumlahLulus+1;
+            }
+            else{
+                jumlahTidakLulus=jumlahTidakLulus+1;
+            }
